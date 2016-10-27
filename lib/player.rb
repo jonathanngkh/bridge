a135cae0219c2be64f9a194ab6d3906029dc00da
Jonathan Ng <jonngkh@hotmail.com>
2016-10-27 23:24:33 +0800

    player knows bid logic

--- lib/player.rb
+++ lib/player.rb
@@ -1,14 +1,21 @@
 class Player
-  attr_accessor :hand, :bid, :playing_card
+  attr_accessor :hand, :bid, :playing_card, :sets_won, :sets_to_win, :sets_to_lose
 
   def initialize
     @hand = []
     @bid = []
     @playing_card = []
+    @sets_won = []
+    @sets_to_win = []
+    @sets_to_lose = []
   end
 
-  def bid_for(bid)
-    self.bid = bid
+  def bid_for(level, suit)
+    self.bid << level
+    self.bid << suit
+    self.sets_to_win = 7 + level - 1
+    self.sets_to_lose = 14 - @sets_to_win
+    "You will need to win #{self.sets_to_win} sets while the opponent will need #{self.sets_to_lose}."
   end
 
   def play(card)
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -13,8 +13,86 @@ describe Player do
     expect(subject).to respond_to(:playing_card)
   end
 
-  it 'can place bid for a suit' do
-    expect{ subject.bid_for(:spades) }.to change { subject.bid }.from([]).to(:spades)
+  it 'holds sets that it won' do
+    expect(subject).to respond_to(:sets_won)
+  end
+
+  it 'can place bid for a level and its suit' do
+    expect{ subject.bid_for(1, :spades) }.to change { subject.bid }.from([]).to([1, :spades])
+  end
+
+  describe 'bid logic' do
+    before do
+      subject.bid_for(level, :spades)
+    end
+
+    context do
+      let(:level) { 1 }
+      it 'knows that level 1 is 7-7' do
+        expect(subject.sets_to_win).to eq 7
+        expect(subject.sets_to_lose).to eq 7
+      end
+    end
+
+    context do
+      let(:level) { 2 }
+      it 'knows that level 1 is 8-6' do
+        expect(subject.sets_to_win).to eq 8
+        expect(subject.sets_to_lose).to eq 6
+      end
+    end
+
+    context do
+      let(:level) { 3 }
+      it 'knows that level 1 is 9-5' do
+        expect(subject.sets_to_win).to eq 9
+        expect(subject.sets_to_lose).to eq 5
+      end
+    end
+
+    context do
+      let(:level) { 4 }
+      it 'knows that level 1 is 10-4' do
+        expect(subject.sets_to_win).to eq 10
+        expect(subject.sets_to_lose).to eq 4
+      end
+    end
+
+    context do
+      let(:level) { 5 }
+      it 'knows that level 1 is 11-3' do
+        expect(subject.sets_to_win).to eq 11
+        expect(subject.sets_to_lose).to eq 3
+      end
+    end
+
+    context do
+      let(:level) { 6 }
+      it 'knows that level 1 is 12-2' do
+        expect(subject.sets_to_win).to eq 12
+        expect(subject.sets_to_lose).to eq 2
+      end
+    end
+
+    context do
+      let(:level) { 7 }
+      it 'knows that level 1 is 13-1' do
+        expect(subject.sets_to_win).to eq 13
+        expect(subject.sets_to_lose).to eq 1
+      end
+    end
+
+    context do
+      let(:level) { 8 }
+      it 'knows that level 1 is 14-0' do
+        expect(subject.sets_to_win).to eq 14
+        expect(subject.sets_to_lose).to eq 0
+      end
+    end
+  end
+
+  it 'notifies itself of the number of sets that need to be won upon bidding' do
+    expect(subject.bid_for(1, :spades)).to eq "You will need to win 7 sets while the opponent will need 7."
   end
 
   it 'can choose a card to play' do
